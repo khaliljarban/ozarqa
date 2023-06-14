209494c505a0c37298e0442034b935e44cb62363
khalil <khalil@r2k.co.il>
2023-06-14 07:01:25 +0300

    documentation

--- ozarqa.js
+++ ozarqa.js
@@ -1,8 +1,61 @@
 class Ozarqa {
     
+    
+
     constructor(args, data) {
 
-        this.svg = document.getElementById(args.svgId);
+        const defaults = {
+            barSpacing: 0
+        };
+
+        //append args to defaults
+        args = { ...defaults,  ...args };
+
+        //verify args paramets
+        if(typeof args.svgSelector=='undefined'){
+            console.warn('Missing svgId parameter.');
+            return;
+        }
+        if(typeof args.chartType=='undefined'){
+            console.warn('Missing chartType parameter.');
+            return;
+        }
+
+        //verify data array
+        if(typeof data=='undefined'){
+            console.warn('Missing data parameter.');
+            return;
+        }
+         
+        if(typeof data.length===0){
+            console.warn('data empty.');
+            return;
+        }
+
+
+         
+
+        //deal with css selector
+        const selector = args.svgSelector.slice(0,1);
+        const selectorName = args.svgSelector.substring(1);
+        switch(selector){
+            case '#':
+                this.svg = document.getElementById(selectorName);
+            break;
+            case '.':
+                this.svg = document.getElementsByClassName(selectorName);
+            break;
+            default:
+                this.svg = document.querySelectorAll(args.svgId);
+            break;
+        }
+        if(typeof this.svg[0]!=='undefined'){
+            this.svg= this.svg[0];
+        }
+         
+        
+
+         
 
          this.args = args;
          this.data = data;
